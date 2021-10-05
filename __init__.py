b484f666f2183dee7328fcafcf764f51cd3ec969
DinossauroBebado <guipsilva32@gmail.com>
2021-10-05 17:25:44 -0300

    add enderecos

---
+++ __init__.py
@@ -0,0 +1,23 @@
+centros_vacina = {
+    "PAVILHÃO DE EVENTOS DO PARQUE BARIGUI": "Alameda Ecológica Burle Marx2518 – Santo Inácio",
+
+    "US VILA DIANA": "Rua René Descartes, 537 – Abranches Boa Vista",
+
+    "US BOM PASTOR ": "Rua José Casagrande, 220 – Vista Alegre",
+
+    "US SANTA QUITÉRIA I": "Rua Divina Providência, 1445 – Santa Quitéria",
+
+    "US FANNY-LINDOIA": "Rua Condes dos Arcos, 295 – Lindoia ",
+
+    "US WALDEMAR MONASTIER": "Rua Romeu Bach, 80 – Boqueirão ",
+
+    "US CAJURU": "Rua Pedro Bochino, 750 – Vila Oficinas ",
+
+    "US SÃO MIGUEL": "Rua Des. Cid Campelo, 8060 – Cidade Industrial",
+
+    "US SÃO JOÃO DEL REY": "Rua Realeza, 259 – Sitio Cercado",
+
+    "US MORADIAS SANTA RITA": "Rua Adriana Zago Bueno, 743 – Tatuquara",
+}
+
+endereco = centros_vacina.values()
